73618318060f0cce00dc99fec2eec4fa36b138ee
mariatheodo <maria.theodo@gmail.com>
2015-11-11 10:47:36 +0200

    correct case sensitive

--- coding.php
+++ coding.php
@@ -28,38 +28,38 @@
 			$freq[chr($i)] = round($val/strlen($m), 2);
 			echo "<br>";
 		}
-		var_dump($chars);
-		echo "<br>";
+		//var_dump($chars);
+		//echo "<br>";
 		asort($freq);
-		var_dump($freq);
-		echo "<br>";
+		//var_dump($freq);
+		//echo "<br>";
 		//προσπάθεια με πίνακες
 		while (count($freq) > 1) {
 			$low = key($freq);
 			
 			next($freq);
 			$high = key($freq);
-			var_dump($low);
-			var_dump($high);
+			//var_dump($low);
+			//var_dump($high);
 			$new_value = $freq[$low] + $freq[$high];
 			array_insert($freq, array("{$low}{$high}"=>$new_value));
-			$chars[$low] = '0';
-			$chars[$high] = '1';
+			$chars[$low] = '1';
+			$chars[$high] = '0';
 			
 			asort($freq);
 			reset($freq);
-			echo "<hr>";
-			var_dump($chars);
-			echo "<br>";
+			//echo "<hr>";
+			//var_dump($chars);
+			//echo "<br>";
 	
-			var_dump($freq);
-			echo "<hr>";
+			//var_dump($freq);
+			//echo "<hr>";
 		}
 		$new_matrix = array();
 		foreach ($letters as $i => $val) {
 			$val = '';
 			foreach ($chars as $j => $bin) {
-				if (stripos($j, chr($i)) !== false ) {
+				if (strpos($j, chr($i)) !== false ) {
 					@$new_matrix[chr($i)] = "$bin".$new_matrix[chr($i)];
 				}
 			}
@@ -69,7 +69,7 @@
 				
 		function array_insert (&$array, $insert_array) { 
 			$first_array = array_splice ($array, 2); 
-			var_dump($first_array);
+			//var_dump($first_array);
 			$array = array_merge ($insert_array, $first_array); 
 		}
 
