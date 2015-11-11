3acac06020240dae8bd90b1a8f6339371f9ff105
mariatheodo <maria.theodo@gmail.com>
2015-11-11 10:15:04 +0200

    'code complete'

--- coding.php
+++ coding.php
@@ -17,14 +17,63 @@
 
 	<?php
 		$letters = count_chars($m, 1); // δημιουργώ πίνακα με όλα τα γράμματα
+		$freq = array();
 		
 		foreach ($letters as $i => $val) {
 			echo chr($i);
 			echo $val; 
 			echo "@";
 			echo round($val/strlen($m), 2);
+			$chars[chr($i)] = $val;
+			$freq[chr($i)] = round($val/strlen($m), 2);
 			echo "<br>";
 		}
+		var_dump($chars);
+		echo "<br>";
+		asort($freq);
+		var_dump($freq);
+		echo "<br>";
+		//προσπάθεια με πίνακες
+		while (count($freq) > 1) {
+			$low = key($freq);
+			
+			next($freq);
+			$high = key($freq);
+			var_dump($low);
+			var_dump($high);
+			$new_value = $freq[$low] + $freq[$high];
+			array_insert($freq, array("{$low}{$high}"=>$new_value));
+			$chars[$low] = '0';
+			$chars[$high] = '1';
+			
+			asort($freq);
+			reset($freq);
+			echo "<hr>";
+			var_dump($chars);
+			echo "<br>";
+	
+			var_dump($freq);
+			echo "<hr>";
+		}
+		$new_matrix = array();
+		foreach ($letters as $i => $val) {
+			$val = '';
+			foreach ($chars as $j => $bin) {
+				if (stripos($j, chr($i)) !== false ) {
+					@$new_matrix[chr($i)] = "$bin".$new_matrix[chr($i)];
+				}
+			}
+		}
+		var_dump($new_matrix);
+		
+				
+		function array_insert (&$array, $insert_array) { 
+			$first_array = array_splice ($array, 2); 
+			var_dump($first_array);
+			$array = array_merge ($insert_array, $first_array); 
+		}
+
+		
 
 	?>
 
