41e917ae7eb10a21002b0df9afadf68e360fc758
mariatheodo <maria.theodo@gmail.com>
2015-11-12 14:06:45 +0200

    final work

--- coding.php
+++ coding.php
@@ -7,6 +7,8 @@
 		<title>Huffman coding</title>
 		<link href="css/style.css" rel="stylesheet">
 		<link href="css/bootstrap.css" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 		<style id="style-1-cropbar-clipper">
 			.en-markup-crop-options {
 				top: 18px;
@@ -27,7 +29,7 @@
 	<body>
 		<div class="container">
 			<div class="row page-header header">
-				<h1>Κωδικοποίηση Huffman</h1>
+				<h1 style="width: 100%;">Κωδικοποίηση Huffman</h1>
 			</div>
 			<br>
 			<br>
@@ -36,7 +38,7 @@
 			<div class="row">
 				<?php include 'php_coding.php';?>
 				
-				<div class="col-md-6, col-lg-6">
+				<div class="row">
 	
 					<p> Το κείμενό σας ήταν το: </p>
 					<div class="panel panel-default">
@@ -44,8 +46,8 @@
 					</div> 
 				</div>
 				
-			</div>
-	<div class="col-md-6, col-lg-6">
+			
+	<div class="row">
 		<p> Οι χαρακτήρες που εμφανίζονται, με τη συχνότητά τους είναι:<p>
 		<table class="table table-hover table-bordered">
 		<thead>
@@ -68,9 +70,45 @@
 	
 		</tbody>
 		</table>
+		
+		<p>Για κωδικοποίηση ASCII χρειάζονται <?php echo 8*strlen($m);?> bits για το μήνυμά σας.</p>
+		<p>Για να κωδικοποιήσετε το συγκεκριμένο μήνυμα μόνο με αλφάβητο <?php $length = ceil(sqrt(count($letters))); echo count($letters);?> χαρακτήρων, θα 
+		χρειαστείτε <?php echo $length*strlen($m);?> bits.</p>
+		<p>Τέλος, αν το συμπιέσετε κατά Huffman θα χρειαστείτε <?php echo $new_length;?> bits.</p>
+		
+		<table class="table table-hover table-bordered">
+		<thead>
+		<tr>
+			<th>Χαρακτήρας</th>
+			<th>Κώδικας Huffman</th>
+			<th>Αριθμός Εμφανίσεων</th>
+			<th>Συχνότητα Εμφάνισης</th>
+		</tr>
+		</thead>
+		<tbody>
+		<?php foreach ($letters as $i => $val) { ?>
+		<tr>
+			<td>"<?php echo chr($i); ?>"</td>
+			<td><?php echo $new_matrix[chr($i)]; ?></td>
+			<td><?php echo $val; ?></td>
+			<td><?php echo round($val/strlen($m), 3); ?></td>
+		</tr>
+		<?php }  ?>
 	
+		</tbody>
+		</table>
     
-      
+
+		<button data-toggle="collapse" data-target="#demo">Πατήστε εδώ για να δείτε το κωδικοποιημένο κείμενο</button>
+
+			<div id="demo" class="collapse">
+				<div class="panel panel-default">
+				<div class="panel-heading">
+				<div class="row-fluid"><?php echo $new_string; ?></div> 
+				</div>
+				</div>
+			</div>
+		
     
 
 	
@@ -79,7 +117,7 @@
 
 	</div>
 
-	
+	</div>
 	
 	</body>
 </html>
--- php_coding.php
+++ php_coding.php
@@ -5,27 +5,20 @@
 		$freq = array();
 		
 		foreach ($letters as $i => $val) {
-			//echo chr($i);
-			//echo $val; 
-			//echo "@";
-			//echo round($val/strlen($m), 2);
+
 			$chars[chr($i)] = $val;
 			$freq[chr($i)] = round($val/strlen($m), 2);
-			//echo "<br>";
+
 		}
-		//var_dump($chars);
-		//echo "<br>";
+
 		asort($freq); //ταξινομώ τον πίνακα ως προς τις τιμές
-		//var_dump($freq);
-		//echo "<br>";
 		
 		while (count($freq) > 1) { 
 			$low = key($freq); //παίρνω το κλειδί της πρώτης θέσης,
 			
 			next($freq); //πάω στην επόμενη
 			$high = key($freq);
-			//var_dump($low);
-			//var_dump($high);
+
 			$new_value = $freq[$low] + $freq[$high]; //προσθέτω τις συχνότητες εμφάνισης
 			array_insert($freq, array("{$low}{$high}"=>$new_value));
 			$chars[$low] = '1'; //δίνω τιμές '1' και '0' σε κάθε χαρακτήρα ή συνδυασμό τους
@@ -33,12 +26,7 @@
 			
 			asort($freq); // ξαναταξινομώ τον πίνακα
 			reset($freq); //μεταφέρω το current στην πρώτη θέση του πίνακα
-			//echo "<hr>";
-			//var_dump($chars);
-			//echo "<br>";
-	
-			//var_dump($freq);
-			//echo "<hr>";
+
 		}
 		$new_matrix = array();
 		foreach ($letters as $i => $val) { //στον πίνακα new_matrix δίνω τιμές σε κάθε χαρακτήρα τον νέο κώδικα
@@ -49,15 +37,38 @@
 				}
 			}
 		}
-		//var_dump($new_matrix);
 		
+		$new_string = huf_string($m, $new_matrix);
+		
+		$new_length = strlen($new_string);
+
 		//αφαιρώ τα δύο πρώτα στοιχεία του πίνακα και στη θέση τους βάζω το συνδυασμό τους		
 		function array_insert (&$array, $insert_array) { 
 			$first_array = array_splice ($array, 2); 
 			//var_dump($first_array);
 			$array = array_merge ($insert_array, $first_array); 
 		}
-
 		
+		function huf_length ($text, $matrix) {
+			$length = 0;
+			$arr = str_split($text);
+			foreach ($arr as $i => $val) {
+				
+				$length += strlen($matrix[$val]);
+				
+			}
+			return $length;
+		}
+		
+		function huf_string ($text, $matrix) {
+			$huf_string = '';
+			$arr = str_split($text);
+			foreach ($arr as $i => $val) {
+				
+				$huf_string .= $matrix[$val];
+				
+			}
+			return $huf_string;
+		}
 
 	?>
